151d1319325ed8e93c02808d5fd2f88dc4bde816
Paulo Ramon <pauloramonlima22@gmail.com>
2020-06-19 14:33:39 -0300

    iniciando a soft mind projeto 1

---
+++ src/main/java/br/com/softmind/sugarbr/SugarbrApplication.java
@@ -0,0 +1,13 @@
+package br.com.softmind.sugarbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarbrApplication.class, args);
+	}
+
+}
